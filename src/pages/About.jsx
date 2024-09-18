08a643730c28add9ccf9c0167e289280fc6016af
Luis Emmanuel <lerodriguezreyes@outlook.com>
2024-09-18 16:23:55 -0400

    About page implemented. TODO: Style page, implement back to home button.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import DirectoraMensaje from "../components/DirectoraMensaje.jsx"
+import Navbar from "../components/Navbar"
 function About() {
   return (
     <div>
-      About page
+      <Navbar />
+      <img src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1726065085/SanusPRWeb/draPagan_wwdxyr.png" alt="Imagen de Dra. Elaine Pagán."/> 
+      <DirectoraMensaje />
+      <button>Regresar al inicio </button>
     </div>
   )
 }
